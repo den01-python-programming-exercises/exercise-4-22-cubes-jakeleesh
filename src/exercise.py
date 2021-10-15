1ea7f3eb5d871c967a806d4d6d70e7af2581c1b1
Jake Lee <jake.lee@fdmgroup.com>
2021-10-15 16:19:18 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    while True:
+        num = input()
+        if (num == "end"):
+            break
+        else:
+            print(int(num) ** 3)
 
 if __name__ == '__main__':
     main()
